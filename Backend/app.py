d0050e0bd9b434205ffb428a072de77530a53073
Peter Yeh <yehyuchun2@gmail.com>
2022-07-11 11:12:20 +0800

    [Backend] Add linters, setup instructions

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 from hashids import Hashids
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
@@ -9,6 +9,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '62e8f6980b724f83f2bb4a'
 CORS(app)
@@ -16,15 +17,19 @@ CORS(app)
 hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
 
-@app.route('/add/teamInfo', methods = ['POST'])
-def add_url():
+@app.route('/add/teamInfo', methods=['POST'])
+def add_team_info():
+    """
+    Parse team info and add it into the database
+    """
+
     conn = get_db_connection()
-    input = request.json.get('input')
-    output = {}
+    user_input = request.json.get('input')
+    parse_team_info(user_input)
 
     if not input:
         return jsonify('Input is empty'), 400
-        
+
     # temp = conn.execute('INSERT INTO (teamInfo) VALUES (?)',
     #                         (url,))
 
@@ -33,20 +38,24 @@ def add_url():
 
     return jsonify('testing'), 200
 
-@app.route('/add/matchResult', methods = ['POST'])
-def add_url():
+
+@app.route('/add/matchResult', methods=['POST'])
+def add_team_score():
+    """
+    Parse team score and add it into database
+    """
     return jsonify('Not implemented'), 200
 
 
+def parse_team_info(user_input):
+    """Parse input based on this format
+    firstTeam 17/05 2
+    secondTeam 07/02 2
+    thirdTeam 24/04 1
+    fourthTeam 24/01 1
 
-
-
-def parse_team_info(input):
-    split_by_next_line = input.splitlines()
-    # firstTeam 17/05 2
-    # secondTeam 07/02 2
-    # thirdTeam 24/04 1
-    # fourthTeam 24/01 1P
+    """
+    split_by_next_line = user_input.splitlines()
     team_info_arr = []
 
     for team_info in split_by_next_line:
@@ -55,10 +64,5 @@ def parse_team_info(input):
         if len(team_info) != 3:
             continue
         team_info_arr.append(split_by_space)
-    
+
     return team_info_arr
-
-        
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Championship Tracker
+
+## Backend
+Run on initialization
+```
+python -m venv env
+source env/bin/activate # Linux
+./env/Scripts/activate # For Windows
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python init_db.py
+```
+
+To start backend
+```
+flask run
+```
+
+Helpful commands
+```
+pip install flask smth...
+pip freeze > requirements.txt
+```
